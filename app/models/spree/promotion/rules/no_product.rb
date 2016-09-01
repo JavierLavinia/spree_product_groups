3a11f402b67feea5049e7facada2405e2f2bc50d
Ruben Sierra <maguilag@gmail.com>
2016-09-01 09:43:29 +0200

    Set rule elegible by selected products

--- app/models/spree/promotion/rules/no_product.rb
+++ app/models/spree/promotion/rules/no_product.rb
@@ -1,5 +1,14 @@
 module Spree
   class Promotion::Rules::NoProduct < Promotion::Rules::Product
+    def eligible?(order, options = {})
+      return false if selected_products(order).empty?
+      if preferred_match_policy == 'all'
+        selected_products(order).all? {|p| order.products.include?(p) }
+      else
+        order.products.any? {|p| selected_products(order).include?(p) }
+      end
+    end
+
     def selected_products(order)
       order.products - eligible_products
     end
